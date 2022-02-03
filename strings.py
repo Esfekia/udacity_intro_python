84e32e397f758fecac5b284953f6020ea623e02e
esfekia <sertac@gmail.com>
2022-02-03 06:53:59 -0500

    Strings, len and use of \ in ' '

---
+++ strings.py
@@ -0,0 +1,6 @@
+# The \ divider allows you to use ' as part of the string.
+salesman = '"I think you\'re an encyclopedia salesman"'
+print(salesman)
+
+# Notice len always results in an integer!
+print(len("ababa") / len("ab"))
