f1b257121ecd1a0a2218e5cfaaa39873467ad41c
esfekia <sertac@gmail.com>
2022-02-07 06:05:25 -0500

    sets examples added.

---
+++ sets_examples.py
@@ -0,0 +1,10 @@
+countries = ['Angola', 'Maldives', 'India', 'United States', 'India', 'Denmark', 'Sweden', 'Ghana', ... 777 more countries not displayed]
+
+print(len(countries))  # 785
+# ['Angola', 'Maldives', 'India', 'United States', 'India']
+print(countries[:5])
+
+country_set = set(countries)
+print(country_set) = 196
+
+country_set.add('Italy')
