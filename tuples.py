2de4c1f8341f7d7e9ec051fdc812811157f862d7
esfekia <sertac@gmail.com>
2022-02-07 05:51:57 -0500

    tuple examples.

---
+++ tuples.py
@@ -0,0 +1,10 @@
+location = (13.4125, 103.866667)
+print("Latitude:", location[0])
+print("Longitude:", location[1])
+
+dimensions = 52, 40, 100
+length, width, height = dimensions
+print("The dimensions are {} x {} x {}".format(length, width, height))
+
+length, width, height = 52, 40, 100
+print("The dimensions are {} x {} x {}".format(length, width, height))
