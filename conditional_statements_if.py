c0279ff71c86d1a6f33bab2555b5063c4c882876
esfekia <sertac@gmail.com>
2022-02-08 05:55:10 -0500

    More if examples added in conditional_statements_if.py

--- conditional_statements_if.py
+++ conditional_statements_if.py
@@ -14,3 +14,48 @@ elif points >= 151 and points <= 180:
 elif points >= 181 and points <= 200:
     result = "Congratulations! You won a penguin!"
 print(result)
+
+# '''
+# You decide you want to play a game where you are hiding
+# a number from someone.  Store this number in a variable
+# called 'answer'.  Another user provides a number called
+# 'guess'.  By comparing guess to answer, you inform the user
+# if their guess is too high or too low.
+
+# Fill in the conditionals below to inform the user about how
+# their guess compares to the answer.
+# '''
+answer = 8  # provide answer
+guess = 8  # provide guess
+
+if answer > guess:
+    result = "Oops!  Your guess was too low."
+elif answer < guess:
+    result = "Oops!  Your guess was too high."
+elif answer == guess:
+    result = "Nice!  Your guess matched the answer!"
+
+print(result)
+
+state = 'CA'
+purchase_amount = 20.00    # a sample state and purchase amount
+
+if state == 'CA':
+    tax_amount = .075
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(
+        state, total_cost)
+
+elif state == 'MN':
+    tax_amount = .095
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(
+        state, total_cost)
+
+elif state == 'NY':
+    tax_amount = .089
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(
+        state, total_cost)
+
+print(result)
