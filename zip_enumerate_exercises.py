2c4295c7704898c846854b2881cadc65df3537ca
Esfekia <sertac@gmail.com>
2022-02-11 10:59:39 -0500

    Four more exercises on use of zip, unzip and enumerate completed.

--- zip_enumerate_exercises.py
+++ zip_enumerate_exercises.py
@@ -37,3 +37,30 @@ names, heights = zip(*cast)
 
 print(names)
 print(heights)
+
+"""Quiz: Enumerate
+Use enumerate to modify the cast list so that each element contains the name followed by the 
+character's corresponding height. For example, the first element of cast should change from 
+"Barney Stinson" to "Barney Stinson 72"."""
+
+cast = ["Barney Stinson", "Robin Scherbatsky",
+        "Ted Mosby", "Lily Aldrin", "Marshall Eriksen"]
+heights = [72, 68, 72, 66, 76]
+# write your for loop here
+i = 0
+while i < len(cast):
+    for name, height in zip(cast, heights):
+        name = "{} {}".format(name, height)
+        cast[i] = name
+        i += 1
+print(cast)
+
+# OR USING ENUMERATE:
+cast = ["Barney Stinson", "Robin Scherbatsky",
+        "Ted Mosby", "Lily Aldrin", "Marshall Eriksen"]
+heights = [72, 68, 72, 66, 76]
+
+for i, character in enumerate(cast):
+    cast[i] = character + " " + str(heights[i])
+
+print(cast)
