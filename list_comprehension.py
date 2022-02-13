54894ad8013744fba2979becd9c8db5c5f8ec88d
esfekia <sertac@gmail.com>
2022-02-13 07:07:43 -0500

    control flow chapter final quiz #1 finished.

--- list_comprehension.py
+++ list_comprehension.py
@@ -32,3 +32,15 @@ scores = {
 
 passed = [key for key in scores if scores[key] >= 65]
 print(passed)
+
+# OR
+scores = {
+    "Rick Sanchez": 70,
+    "Morty Smith": 35,
+    "Summer Smith": 82,
+    "Jerry Smith": 23,
+    "Beth Smith": 98
+}
+
+passed = [name for name, score in scores.items() if score >= 65]
+print(passed)
