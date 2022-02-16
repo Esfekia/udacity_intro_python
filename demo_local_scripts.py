68062d01420eccadeb9039e147ae3cc62fde8d06
Esfekia <sertac@gmail.com>
2022-02-16 18:01:09 -0500

    Added a 3-word password generator that takes words from a text file randomly and combines them.

--- demo_local_scripts.py
+++ demo_local_scripts.py
@@ -1,9 +1,14 @@
-import useful_functions
+import useful_functions as uf
+
 scores = [88, 92, 79, 93, 85]
 
-mean = useful_functions.mean(scores)
-curved = useful_functions.add_five(scores)
-mean_c = useful_functions.mean(curved)
+mean = uf.mean(scores)
+curved = uf.add_five(scores)
+
+mean_c = uf.mean(curved)
 
 print("Scores:", scores)
-print("Original Mean: ", mean, " New Mean: ", mean_c)
+print("Original Mean:", mean, " New Mean:", mean_c)
+
+print(__name__)
+print(uf.__name__)
