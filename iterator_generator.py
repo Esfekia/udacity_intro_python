afee85beebb9afb054f9ca0c51fa5b26148b72af
esfekia <sertac@gmail.com>
2022-02-18 06:33:52 -0500

    iterator generator example using udacity program section names completed.

---
+++ iterator_generator.py
@@ -0,0 +1,13 @@
+lessons = ["Why Python Programming", "Data Types and Operators",
+           "Control Flow", "Functions", "Scripting"]
+
+
+def my_enumerate(iterable, start=0):
+    # Implement your generator function here
+    for item in iterable:
+        yield start, item
+        start += 1
+
+
+for i, lesson in my_enumerate(lessons, 1):
+    print("Lesson {}: {}".format(i, lesson))
